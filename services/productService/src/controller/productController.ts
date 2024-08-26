921f36ab5eb29268a8ed3ff63c272ac054dad7d9
BAHIRABDULLA <bahirk880@gmail.com>
2024-08-26 17:06:44 +0530

    added more product functionalities

--- services/productService/src/controller/productController.ts
+++ services/productService/src/controller/productController.ts
@@ -1,6 +1,19 @@
 import { Request,Response } from "express"
 import Product from "../model/productModel"
 
+
+//      all product geting functionality
+const getProducts = async(req:Request,res:Response)=>{
+    try {
+        const products = await Product.find()
+        return res.status(200).json({products})
+    } catch (error) {
+        console.error('Error founded in product getting side ',error);
+    }
+}
+
+
+//      product adding functionality
 const productAdd = async(req:Request,res:Response)=>{
     const {name,category,quantity,price}:{name:string;category:string;quantity:number;price:number} =req.body
     try {
@@ -23,6 +36,8 @@ const productAdd = async(req:Request,res:Response)=>{
         
     }
 }
+
+//       product edit functionality
 const productEdit = async(req:Request,res:Response)=>{
     const {name,category,price,quantity}:{name:string;category:string;price:number;quantity:number} = req.body
     const productId = req.params._id
@@ -37,6 +52,7 @@ const productEdit = async(req:Request,res:Response)=>{
             {name,category,price,quantity},{new:true}
         )
         console.log(updatedProduct,'updatedProduct')
+        return res.status(200).json({Products:updatedProduct})
     } catch (error) {
         console.error('Cannot update product',error);
         res.status(500).json({message:"Product cannot be updated"})
@@ -44,7 +60,21 @@ const productEdit = async(req:Request,res:Response)=>{
     }
 }
 
+
+//         product delete functionality
+const productDelete = async(req:Request,res:Response)=>{
+    const productId = req.params._id
+    try {
+        await Product.findByIdAndDelete(productId)
+        res.status(200).json({message:"product delete successfully"})
+    } catch (error) {
+        console.error('Error deleting product',error);
+        
+    }
+}
 export default {
+    getProducts,
     productAdd,
-    productEdit
+    productEdit,
+    productDelete
 }
--- services/productService/src/routes/productRoute.ts
+++ services/productService/src/routes/productRoute.ts
@@ -3,8 +3,12 @@ import express from 'express'
 const router = express.Router()
 
 import productController from '../controller/productController'
-const {productAdd,productEdit}=productController
+const {productAdd,productEdit,getProducts,productDelete}=productController
+
+router.get('/products',getProducts)
 router.post('/productAdd',productAdd)
+router.post('/productEdit/:id',productEdit)
+router.delete('/productDelete/:id',productDelete)
 
 
 export default router
